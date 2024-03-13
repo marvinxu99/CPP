2512ab7c101bb037e5a92173f44e06c1666d25aa
Marvin Xu <marvinxu99@hotmail.com>
2024-03-12 20:22:22 -0700

    ...

---
+++ learning/ppp_cpp/GUI_fltk/graph_gui/Simple_window.h
@@ -0,0 +1,48 @@
+
+#include "GUI.h"	// for Simple_window only (doesn't really belong in Window.h)
+
+using namespace Graph_lib;
+
+// Simple_window is basic scaffolding for ultra-simple interaction with graphics
+// it provides one window with one "next" button for ultra-simple animation
+
+struct Simple_window : Window {
+	Simple_window(Point xy, int w, int h, const string& title)
+		: Window{ xy,w,h,title },
+		button_pushed{ false },
+			//next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) 
+		next_button{ Point(x_max() - 70,0), 70, 20, "Next",
+			[](Address, Address pw) {
+				reference_to<Simple_window>(pw).next();
+			}
+		}
+	{
+		attach(next_button); 
+	}
+	
+	void wait_for_button()
+	// modified event loop
+	// handle all events (as per default), but quit when button_pushed becomes true
+	// this allows graphics without control inversion
+	{
+		while (!button_pushed) Fl::wait();
+		button_pushed = false;
+		Fl::redraw();
+	}
+
+private:
+	bool button_pushed;
+	Button next_button;
+
+	// Another option is to use a lambda function for call back
+	//static void cb_next(Address, Address addr) // callback for next_button - call Simple_window::next() for the window located at pw
+	//{ 
+	//	reference_to<Simple_window>(addr).next(); 
+	//}
+	//{
+	//	static_cast<Simple_window*>(addr)->next();
+	//}
+
+	void next() { button_pushed = true; }
+
+};
